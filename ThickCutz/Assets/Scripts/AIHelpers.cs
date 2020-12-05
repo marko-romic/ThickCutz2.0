f1bd628a95f3feb7eb8fc3944a476271355e4abe
Ross Curry <rossacurry@gmail.com>
2020-12-05 02:03:23 -0500

    AI State Machine Prototype
    -Template for state machine

---
+++ ThickCutz/Assets/Scripts/AIHelpers.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class AIHelpers
+{
+    static private float maxWanderDuration = 2.0f;
+    static private float wanderCounter = 0.0f;
+    static private System.Random r = new System.Random();
+
+    public class MovementResult
+    {
+        public Vector3 newPosition = Vector3.zero;
+        public Vector3 newOrientation = Vector3.zero;
+    }
+
+    public class InputParameters
+    {
+        public InputParameters(Transform current, Transform target, float updateDelta, float speed)
+        {
+            currentTransform = current;
+            targetTransform = target;
+            currentUpdateDuration = updateDelta;
+            maxSpeed = speed;
+        }
+
+        public InputParameters(InputParameters o)
+        {
+            currentTransform = o.currentTransform;
+            targetTransform = o.targetTransform;
+            currentUpdateDuration = o.currentUpdateDuration;
+            maxSpeed = o.maxSpeed;
+        }
+
+        public InputParameters()
+        {
+            currentUpdateDuration = 0.0f;
+            maxSpeed = 1.0f;
+        }
+
+        public Transform currentTransform;
+        public Transform targetTransform;
+        public float currentUpdateDuration;
+        public float maxSpeed;
+    }
+
+    public enum MovementBehaviors
+    {
+        None,
+        SeekKinematic,
+        FleeKinematic,
+        WanderKinematic
+    }
+
+    internal static void SeekKinematic(InputParameters inputData, ref MovementResult result)
+    {
+        Vector3 directionToTarget = inputData.targetTransform.position - inputData.currentTransform.position;
+        directionToTarget.Normalize();
+
+        result.newPosition = inputData.currentTransform.position + (directionToTarget * inputData.maxSpeed * inputData.currentUpdateDuration);
+    }
+
+    internal static void FleeKinematic(InputParameters inputData, ref MovementResult result)
+    {
+        Vector3 directionToTarget = inputData.currentTransform.position - inputData.targetTransform.position;
+        directionToTarget.Normalize();
+
+        result.newPosition = inputData.currentTransform.position + (directionToTarget * inputData.maxSpeed * inputData.currentUpdateDuration);
+    }
+
+    internal static void WanderKinematic(InputParameters inputData, ref MovementResult result)
+    {
+        int range = 5;
+        
+        wanderCounter += inputData.currentUpdateDuration;
+        if (wanderCounter > maxWanderDuration)
+        {
+            Vector3 randomTarget = inputData.targetTransform.position;
+            randomTarget.x += (float)r.NextDouble() * range;
+            randomTarget.z += (float)r.NextDouble() * range;
+            inputData.targetTransform.position = randomTarget;
+            wanderCounter = 0.0f;
+        }
+
+        SeekKinematic(inputData, ref result);
+    }
+}
---
+++ ThickCutz/Assets/Scripts/Robot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public AIHelpers.MovementBehaviors activeMovementBehavior = AIHelpers.MovementBehaviors.None;
+    public GameObject targetObject;
+    public GameObject seekObject;
+    public float maxSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AIHelpers.InputParameters inputData = new AIHelpers.InputParameters(gameObject.transform, targetObject.transform, Time.deltaTime, maxSpeed);
+        AIHelpers.MovementResult movementResult = new AIHelpers.MovementResult();
+
+        switch (activeMovementBehavior)
+        {
+            case AIHelpers.MovementBehaviors.FleeKinematic:
+                AIHelpers.FleeKinematic(inputData, ref movementResult);
+                break;
+            case AIHelpers.MovementBehaviors.SeekKinematic:
+                AIHelpers.SeekKinematic(inputData, ref movementResult);
+                break;
+            case AIHelpers.MovementBehaviors.WanderKinematic:
+
+                AIHelpers.WanderKinematic(inputData, ref movementResult);
+
+                break;
+            default:
+                //AIHelpers.SeekKinematic(inputData, ref movementResult);
+                movementResult.newPosition = transform.position;
+                break;
+        }
+
+        gameObject.transform.position = movementResult.newPosition;
+    }
+
+    public void ActivateWander()
+    {
+        activeMovementBehavior = AIHelpers.MovementBehaviors.WanderKinematic;
+    }
+
+    public void ActivateSeek()
+    {
+        targetObject = seekObject;
+        activeMovementBehavior = AIHelpers.MovementBehaviors.SeekKinematic;
+    }
+
+    public void ActivateLeave()
+    {
+        activeMovementBehavior = AIHelpers.MovementBehaviors.FleeKinematic;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")){
+            Destroy(gameObject);
+        }
+    }
+
+}
